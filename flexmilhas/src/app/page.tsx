6992745d06c6c5bb029a1ed4396c59d2e415bb09
Julia Borges <juliaborgess18@gmail.com>
2024-10-27 20:59:28 -0300

    ajustando texto da pagina inicial

--- flexmilhas/src/app/page.tsx
+++ flexmilhas/src/app/page.tsx
@@ -7,13 +7,13 @@ const NewArrivals = () => {
   return (
     <div className="bg-white">
       <Navbar></Navbar>  
+      <h2 className="text-3xl font-medium text-center mt-20">Viaje pelo Brasil e Mundo com ofertas imperdíveis!</h2>  
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-
         <div className="flex space-x-8">
-          <aside className="w-1/4">
+        <aside className="w-1/4">
             <div className="mb-8">
+              <ul className="space-y-2">
               <h2 className="text-lg font-medium mt-20">Escolha seu Destino</h2>
-              <ul className="mt-5 space-y-2">
                 {["Brasil", "Estados Unidos", "França", "Japão", "Alemanha", "Canadá", "Itália", "Espanha", "Austrália", "Nova Zelândia", "Argentina", "Chile", "México", "Portugal", "Grécia", "Turquia", "China", "Índia", "África do Sul", "Egito", "Tailândia", "Indonésia", "Suíça", "Holanda", "Suécia", "Noruega", "Dinamarca", "Coreia do Sul", "Arábia Saudita", "Emirados Árabes Unidos"].map((color) => (
                   <li key={color}>
                     <label className="flex items-center">
@@ -25,7 +25,6 @@ const NewArrivals = () => {
               </ul>
             </div>
           </aside>
-
           <Catalogo></Catalogo>
         </div>
       </main>
--- flexmilhas/src/components/navbar/index.tsx
+++ flexmilhas/src/components/navbar/index.tsx
@@ -1,29 +1,27 @@
-
-export function Navbar(){
+export function Navbar() {
   return (
-    <header className="lg:px-16 px-4 bg-violet-700 text-white flex flex-wrap items-center py-4 shadow-md">
-        <div className="flex-1 flex justify-between items-center">
-            <a href="#" className="text-xl">FlexMilhas</a>
-        </div>
-
-        <label htmlFor="menu-toggle" className="pointer-cursor md:hidden block">
-          <svg className="fill-current text-gray-900"
-            xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
-            <title>menu</title>
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
-          </svg>
-        </label>
+    <header className="lg:px-16 px-4 bg-violet-700 text-white flex items-center py-4 shadow-md">
+      <div className="flex-1 flex justify-between items-center">
+        <a href="#" className="text-xl">FlexMilhas</a>
+        
         <input className="hidden" type="checkbox" id="menu-toggle" />
+        
+        <nav className="flex items-center space-x-4">
+          <ul className="hidden md:flex items-center justify-between text-base text-white pt-4 md:pt-0">
+            <li>
+              <a className="md:p-4 py-3 px-0 block" href="/usuario/acessar-conta">
+                Fazer login
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
 
-        <div className="hidden md:flex md:items-center md:w-auto w-full" id="menu">
-            <nav>
-                <ul className="md:flex items-center justify-between text-base text-white pt-4 md:pt-0">
-                    <li><a className="md:p-4 py-3 px-0 block" href="/usuario/acessar-conta">Fazer login</a></li>
-                </ul>
-            </nav>
-        </div>
+      <div className="md:hidden">
+        <a className="py-3 px-4 block" href="/usuario/acessar-conta">
+          Fazer login
+        </a>
+      </div>
     </header>
-
-    
-  )
+  );
 }
